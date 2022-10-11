6b3f95d4c4adb4211dcad3cf5e2dcec79fafd8f2
Sean Park <chanseanpark@gmail.com>
2022-10-10 20:14:16 -0700

    updated port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
