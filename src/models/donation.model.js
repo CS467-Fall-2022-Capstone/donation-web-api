1d4ce0344ee450a2563cb89a521b849b7827890f
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-02 16:56:16 -0500

    fill out Donation model

--- src/models/donation.model.js
+++ src/models/donation.model.js
@@ -6,6 +6,28 @@ import mongoose, { Schema } from 'mongoose';
  */
 
 const DonationSchema = new Schema({
+    student_id: {
+        type: String,
+        trim: true,
+        required: [true, 'student_id is required']
+    },
+    supply_id: {
+        type: String,
+        required: [true, 'supply_id is required']
+    },
+    item: {
+        type: String,
+        required: [true, 'item is required']   
+    },
+   quantityDonated: {
+        type: Number,
+        required: [true,'quantityDonated is required']
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    updated: Date,
 });
 
 export default mongoose.model('Donation', DonationSchema);
