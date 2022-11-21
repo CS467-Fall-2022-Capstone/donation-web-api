9ad9a757d7c3a119dcc66c9ca9562cf771ba2d22
Sean Park <chanseanpark@gmail.com>
2022-11-21 00:12:44 -0800

    endpoint refactors, working bulk update and create for donations

--- src/controllers/donation.controller.js
+++ src/controllers/donation.controller.js
@@ -2,58 +2,86 @@ import Donation from '../models/donation.model.js';
 import errorHandler from '../helpers/dbErrorHandler.js';
 import Student from '../models/student.model.js';
 import Supply from '../models/supply.model.js';
+import Teacher from '../models/teacher.model.js';
 
 /**
  * Controller functions to be mounted on the Donation route
  */
 
-/**
- * Use to load the donation object into the Express req object BEFORE
- * propogating to the next function thats specific to the request
- * that came in
- */
- const donationByID = async (req, res, next, id) => {
-    try {
-        let donation = await Donation.findById(id);
-        if (!donation) {
-            return res.status(404).json({
-                error: 'Donation not found',
-            });
+const bulkWriteDonations = async (req, res) => {
+    const student_id = req.params.studentId;
+    const donations = req.body;
+    const donationBulkOperations = donations.map(createBulkWriteOperation);
+    const filteredBulkOperations = donationBulkOperations.filter(
+        (item) => item !== undefined
+    );
+    const results = await Donation.bulkWrite(filteredBulkOperations);
+    // returns an array of ids that were inserted after the bulkWrite
+    const newDonations = results.getInsertedIds();
+    if (newDonations.length > 0) {
+        // push the new donation ids to Student.donations and Supply.donations
+        const student = await Student.findByIdAndUpdate(
+            student_id,
+            { $push: { donations: { $each: newDonations } } },
+            { new: true }
+        );
+        const bulkUpdateOps = await addDonationToSupplies(newDonations);
+        // Insert all new donations into the supplys.donation array
+        const supplyUpdates = await Supply.bulkWrite(bulkUpdateOps);
+        console.log(
+            `${supplyUpdates.modifiedCount} / ${bulkUpdateOps.length} supplies updated`
+        );
+    }
+    return res.status(200).send('Success');
+};
+
+const addDonationToSupplies = async (donations) => {
+    // Find all the new donations
+    const donationsWithSupplyId = await Donation.find({
+        _id: { $in: donations },
+    });
+    // Create updateOne operations for the Supplies in donation.supply_id
+    const supplyUpdates = donationsWithSupplyId.map((item) => {
+        return {
+            updateOne: {
+                filter: { _id: item.supply_id },
+                update: { $push: { donations: item._id } },
+            },
+        };
+    });
+    return supplyUpdates;
+};
+
+const createBulkWriteOperation = (item) => {
+    if (item.update) {
+        // donation exists
+        if (item.donationFields.quantityDonated === 0) {
+            // if its existing donation and the student set it to 0, then remove the donation
+            return {
+                deleteOne: { filter: item.donationFields.donation_id },
+            };
+        } else {
+            // quantityDonated > 0 so update
+            return {
+                updateOne: {
+                    filter: { _id: item.donationFields.donation_id },
+                    update: {
+                        quantityDonated: item.donationFields.quantityDonated,
+                    },
+                },
+            };
         }
-        let supply_id = donation.supply_id;
-        let supply = await Supply.findById(supply_id);
-        let item = supply.item;
-        donation.item = item;
-        req.donation = donation;
-        next();
-    } catch (err) {
-        return res.status(400).json({
-            error: 'Could not retrieve this donation',
-        });
+    } else {
+        // item.update is false so its a new donation, create insertOne operation
+        // only create a new donation for items where the student inputted a non-zero value
+        if (item.donationFields.quantityDonated > 0) {
+            return {
+                insertOne: {
+                    document: { ...item.donationFields },
+                },
+            };
+        }
     }
 };
 
-const create = async (req,res) => {
-    try {
-        const donation = await Donation.create(req.body);
-        const student_id = donation.student_id;
-        const student = await Student.findById(student_id);
-        student.donations.push(donation._id);
-        student.save();
-        const supply_id = donation.supply_id;
-        const supply = await Supply.findById(supply_id);
-        supply.donations.push(donation._id);
-        await supply.save();
-        return res.status(201).json(donation.toJSON());
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err),
-        });
-    }
-};
-
-const read = (req, res) => {
-    return res.json(req.donation.toJSON());
-};
-
-export default { donationByID, create, read };
+export default { bulkWriteDonations };
