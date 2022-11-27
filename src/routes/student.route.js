aaf5fff65ab26d1989a6d58fa3ecbd1c5fc64b40
Sean Park <chanseanpark@gmail.com>
2022-11-27 15:13:30 -0800

    re-order route hierarchy

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -5,22 +5,22 @@ const router = express.Router();
 
 router.route('/students').post(studentCtrl.create);
 
+// Find student by donation code entered in modal form
+router
+    .route('/students/:donationCode/donations')
+    .get(studentCtrl.studentByDonationCode);
+
 router
     .route('/students/:studentId')
     .get(studentCtrl.read)
     .patch(studentCtrl.update)
     .delete(studentCtrl.remove);
 
-// Find student by donation code entered in modal form
-router
-    .route('/students/:donationCode/donations')
-    .get(studentCtrl.studentByDonationCode)
-
 /**
-* Preloads user into the Express req object BEFORE
-* propogating to the next function thats specific to the request
-* that came in
-*/
+ * Preloads user into the Express req object BEFORE
+ * propogating to the next function thats specific to the request
+ * that came in
+ */
 router.param('studentId', studentCtrl.studentByID);
 
 export default router;
